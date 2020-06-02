73533a07f380a2de4a59131af046b56b19db80a6
Marco Amorim <mapamarco@gmail.com>
2020-06-02 21:28:29 +0100

    Fix performance test

--- test/test_perf.cpp
+++ test/test_perf.cpp
@@ -1,18 +1,21 @@
 #include <gtest/gtest.h>
 
-#include <chrono>
-using namespace std::chrono;
-
 #include <naive_gbe/benchmark.hpp>
 #include <naive_gbe/emulator.hpp>
 using namespace naive_gbe;
+
+#ifdef _DEBUG
+	#define BASELINE_MUL_FACTOR	150
+#else
+	#define BASELINE_MUL_FACTOR	5
+#endif
 
 TEST(performance, bootstrap)
 {
 	emulator emu;
 
 	emu.load_cartridge({
-		0x10,				// stop
+		0x10,				// STOP
 	});
 
 	auto& cpu = emu.get_cpu();
@@ -35,16 +38,16 @@ TEST(performance, bootstrap)
 		num_cycles += cpu.get_cycle();
 	});
 
-	//std::size_t total = 0;
-	//auto baseline = b.run("baseline", [&]
-	//{
-	//	for (auto i = 0; i < 1e5; ++i)
-	//		++total;
-	//});
-	//
-	//EXPECT_EQ(total, 1e5 * num_samples);
-	//EXPECT_EQ(num_steps, 24902 * num_samples);
-	//EXPECT_EQ(num_cycles, 231884 * num_samples);
-	//EXPECT_EQ(cpu.get_register(lr35902::r16::PC), 0x0101);
-	//EXPECT_LT(result.average, baseline.average * 5.0);
+	std::size_t total = 0;
+	auto baseline = b.run("baseline", [&]
+	{
+		for (auto i = 0; i < 1e5; ++i)
+			++total;
+	});
+	
+	EXPECT_EQ(total, 1e5 * num_samples);
+	EXPECT_EQ(num_steps, 24902 * num_samples);
+	EXPECT_EQ(num_cycles, 231884 * num_samples);
+	EXPECT_EQ(cpu.get_register(lr35902::r16::PC), 0x0101);
+	EXPECT_LT(result.average, baseline.average * BASELINE_MUL_FACTOR);
 }
